f3b695a7632b5edfe6d47630780f41da4becba1c
Rahul Punyani <punyanirahul17@gmail.com>
2019-05-25 22:34:50 +0530

    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {MatButtonModule, MatCheckboxModule , MatMenuModule, MatSidenavModule, MatSelectModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatSelectModule
+   
+
   ],
   providers: [],
   bootstrap: [AppComponent]
